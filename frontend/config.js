fc23bc909f3d417f9db62c7be764c1a8568e3a5c
Alistair McCormick <amccormi@nhm.org>
2019-01-06 09:44:35 -0800

    more changes

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = `https://palazar-react-prod.herokuapp.com/`;
+export const prodEndpoint = `https://palazar-yoga-prod.herokuapp.com/`;
 export const perPage = 10;
