c35738bcd2cb2c0091980746fcd97355d0c93a87
Alistair McCormick <amccormi@nhm.org>
2019-01-23 17:02:46 -0800

    more info making the money stuff live

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -2,12 +2,12 @@ import Link from 'next/link';
 
 const About = props => (
 	<div>
-    <p> We are a company</p>
+    <p>We are a company</p>
     <p>We want to make booking appointments easier for you and the creators we work with.</p>
     <p>We also want to make sure the creators get all the money they deserve so they can keep doing what they do.</p>
-    <p>We charge a fee so that we can take processing charges, pay for servers and keep your data and our data secure</p>
+    <p>We charge a fee so that we can take on processing charges, pay for servers and keep your data and our data secure.</p>
     <p> THANK YOU! </p>
-    <p>Please reach out at palazar@gmail.com </p>
+    <p>Please reach out at alistm12@gmail.com </p>
 	</div>
 )
 
--- frontend/pages/terms.js
+++ frontend/pages/terms.js
@@ -2,7 +2,10 @@ import Link from 'next/link';
 
 const Terms = props => (
 	<div>
-    <p>Payment processing services for [account holder term, e.g. drivers or sellers] on [platform name] are provided by Stripe and are subject to the Stripe Connected Account Agreement, which includes the Stripe Terms of Service (collectively, the “Stripe Services Agreement”). By agreeing to [this agreement / these terms / etc.] or continuing to operate as a [account holder term] on [platform name], you agree to be bound by the Stripe Services Agreement, as the same may be modified by Stripe from time to time. As a condition of [platform name] enabling payment processing services through Stripe, you agree to provide [platform name] accurate and complete information about you and your business, and you authorize [platform name] to share it and transaction information related to your use of the payment processing services provided by Stripe.</p>
+    <p>Payment processing services for sellers on palazar are provided by Stripe and are subject to the Stripe Connected Account Agreement, which includes the Stripe Terms of Service (collectively, the “Stripe Services Agreement”). By agreeing to this aggreement or continuing to operate as a seller on palazar, you agree to be bound by the Stripe Services Agreement, as the same may be modified by Stripe from time to time. As a condition of palazar enabling payment processing services through Stripe, you agree to provide palazar accurate and complete information about you and your business, and you authorize palazar to share it and transaction information related to your use of the payment processing services provided by Stripe.</p>
+
+		<h2>Refunds</h2>
+		<p>Palazar does not pay any refunds for services paid. It is a aggreement between the buyer and the seller to finish the appointment request in person. Palazar will not refund orders.</p>
 	</div>
 )
 
