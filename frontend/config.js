9b62812df339d56285518504c28d551bc899b01c
Alistair McCormick <amccormi@nhm.org>
2019-01-28 12:46:52 -0800

    connie

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,6 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = `https://palazar-yoga-prod.herokuapp.com/`;
+export const prodEndpoint = `palazar-yoga-prod.herokuapp.com/`;
 export const perPage = 10;
 export const prodStripe = `pk_live_rEOt0HJLqQuzVcdyuJ8AppBZ`
 export const devStripe = `pk_test_K3j8JU1o7WrtlpA5zY8bavrR`;
